991647757a23a4ab948e1bfe325090e1770c50d7
fluentlyy <108574339+fluentlyy@users.noreply.github.com>
2026-09-23 23:45:00 +0300

    123

---
+++ OP/prac/kr-1/ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int num = 25;
+    int rest, result;
+
+
+    printf("%d", result);
+    
+    /* while(num > 0){
+        
+    } */
+
+    
+
+    return 0;
+}
